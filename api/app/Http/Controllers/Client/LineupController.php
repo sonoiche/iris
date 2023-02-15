2b8af53db567e31f828476fc2050a0518a3ed558
Jelson Lanto <jelsonlanto@Jelsons-iMac.local>
2023-02-16 04:57:36 +0800

    final revisions

--- api/app/Http/Controllers/Client/LineupController.php
+++ api/app/Http/Controllers/Client/LineupController.php
@@ -84,7 +84,7 @@ class LineupController extends Controller
         }
 
         if($status_id == Lineup::FOR_INTERVIEW) {
-            $ids = explode(',', $applicant_ids);
+            return $ids = explode(',', $applicant_ids);
             $lineups = Lineup::whereIn('applicant_id', $ids)->get();
             $interview_date = Carbon::parse($request['interview_date'])->addDay()->format('Y-m-d').' '.$request['interview_time'].':00';
             $data = [];
