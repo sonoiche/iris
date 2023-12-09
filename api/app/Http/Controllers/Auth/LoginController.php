aaa774e1e40ef3c98d704f379398c8d58ce06735
Jelson Lanto <jelsonlanto@Jelsons-iMac.local>
2023-12-09 22:36:03 +0800

    debugging

--- api/app/Http/Controllers/Auth/LoginController.php
+++ api/app/Http/Controllers/Auth/LoginController.php
@@ -60,83 +60,84 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        $this->validator($request->all())->validate();
-        $datetimenow = Carbon::now()->addDay()->format('Y-m-d h:i:s');
-        $request->validate([
-            'email' => ['required','email'],
-            'password' => ['required']
-        ]);
+        return $request->all();
+        // $this->validator($request->all())->validate();
+        // $datetimenow = Carbon::now()->addDay()->format('Y-m-d h:i:s');
+        // $request->validate([
+        //     'email' => ['required','email'],
+        //     'password' => ['required']
+        // ]);
 
-        $user = User::where('email', $request['email'])->first();
-        if(!$user || !Hash::check($request['password'], $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credential is incorrect.']
-            ]);
-        }
-
-        $oauth = DB::table('oauth_access_tokens')->where('user_id', $user->id)
-            ->where('expires_at', '>', $datetimenow)
-            ->first();
-
-        if(!empty($oauth)){
-            if($user->access_token) {
-                $data['token'] = $user->access_token;
-            } else {
-                $data['token'] = $token = $user->createToken($user->name)->accessToken;
-                User::where('id', $user->id)->update(['access_token' => $token]);
-            }
-        } else {
-            $data['token'] = $token = $user->createToken($user->name)->accessToken;
-            User::where('id', $user->id)->update(['access_token' => $token]);
-        }
-
-        if($user->two_factor_secret) {
-            $data['qrcode'] = $user->twoFactorQrCodeSvg();
-            $data['code']   = '';
-            foreach (json_decode(decrypt($user->two_factor_recovery_codes, true)) as $code) {
-                $data['code'] = trim($code);
-                break;
-            }
-        }
-
-        // if($user->sms_authentication) {
-        //     $user->sms_code = $this->generateOtp();
-        //     $user->save();
-
-        //     // send OTP
-        //     $phone_number = (string) PhoneNumber::make($user->sms_auth_number)->ofCountry('PH');
-        //     $otp_password = $user->sms_code;
-
-        //     $sid    = "ACc27ac3a6b54f5abf0471c2b1c5ff4376"; 
-        //     $token  = "47cabe340916bca8d41f764897fbc9e7"; 
-        //     $twilio = new Client($sid, $token); 
-            
-        //     $twilio->messages->create($phone_number,
-        //         [
-        //             "messagingServiceSid" => "MG2e9435f1a0ed02995b56aa45c6247e82",      
-        //             "body" => "IRIS Online one time password. Your OTP is ".$otp_password.". To continue logging in to your account, input this 6 digit code." 
-        //         ] 
-        //     ); 
+        // $user = User::where('email', $request['email'])->first();
+        // if(!$user || !Hash::check($request['password'], $user->password)) {
+        //     throw ValidationException::withMessages([
+        //         'email' => ['The provided credential is incorrect.']
+        //     ]);
         // }
 
-        $timenow = Carbon::now();
-        if(isset($user->two_factor_until) && $timenow > $user->two_factor_until) {
-            $user->two_factor_until = null;
-            $user->save();
-        }
+        // $oauth = DB::table('oauth_access_tokens')->where('user_id', $user->id)
+        //     ->where('expires_at', '>', $datetimenow)
+        //     ->first();
 
-        // insert to activity log
-        $activity = new ActivityLog;
-        $activity->user_id          = $user->id;
-        $activity->username         = $user->fname.' '.$user->lname;
-        $activity->activity_type    = 'access';
-        $activity->ip_address       = $this->getIp();
-        $activity->save();
+        // if(!empty($oauth)){
+        //     if($user->access_token) {
+        //         $data['token'] = $user->access_token;
+        //     } else {
+        //         $data['token'] = $token = $user->createToken($user->name)->accessToken;
+        //         User::where('id', $user->id)->update(['access_token' => $token]);
+        //     }
+        // } else {
+        //     $data['token'] = $token = $user->createToken($user->name)->accessToken;
+        //     User::where('id', $user->id)->update(['access_token' => $token]);
+        // }
 
-        $data['user'] = $user;
-        $data['two_factor_until'] = $user->two_factor_until;
+        // if($user->two_factor_secret) {
+        //     $data['qrcode'] = $user->twoFactorQrCodeSvg();
+        //     $data['code']   = '';
+        //     foreach (json_decode(decrypt($user->two_factor_recovery_codes, true)) as $code) {
+        //         $data['code'] = trim($code);
+        //         break;
+        //     }
+        // }
 
-        return response()->json($data);
+        // // if($user->sms_authentication) {
+        // //     $user->sms_code = $this->generateOtp();
+        // //     $user->save();
+
+        // //     // send OTP
+        // //     $phone_number = (string) PhoneNumber::make($user->sms_auth_number)->ofCountry('PH');
+        // //     $otp_password = $user->sms_code;
+
+        // //     $sid    = "ACc27ac3a6b54f5abf0471c2b1c5ff4376"; 
+        // //     $token  = "47cabe340916bca8d41f764897fbc9e7"; 
+        // //     $twilio = new Client($sid, $token); 
+            
+        // //     $twilio->messages->create($phone_number,
+        // //         [
+        // //             "messagingServiceSid" => "MG2e9435f1a0ed02995b56aa45c6247e82",      
+        // //             "body" => "IRIS Online one time password. Your OTP is ".$otp_password.". To continue logging in to your account, input this 6 digit code." 
+        // //         ] 
+        // //     ); 
+        // // }
+
+        // $timenow = Carbon::now();
+        // if(isset($user->two_factor_until) && $timenow > $user->two_factor_until) {
+        //     $user->two_factor_until = null;
+        //     $user->save();
+        // }
+
+        // // insert to activity log
+        // $activity = new ActivityLog;
+        // $activity->user_id          = $user->id;
+        // $activity->username         = $user->fname.' '.$user->lname;
+        // $activity->activity_type    = 'access';
+        // $activity->ip_address       = $this->getIp();
+        // $activity->save();
+
+        // $data['user'] = $user;
+        // $data['two_factor_until'] = $user->two_factor_until;
+
+        // return response()->json($data);
     }
 
     public function logout(Request $request)
